4e12e3a4e5e5a891a3e2d656e05e5a9951bffd91
shiron <shiron4710.dev@gmail.com>
2023-10-04 08:24:33 +0900

    feat: update timeout

--- app/src/main/java/dev/shiron/smallstride/domain/ApiClass.kt
+++ app/src/main/java/dev/shiron/smallstride/domain/ApiClass.kt
@@ -1,11 +1,13 @@
 package dev.shiron.smallstride.domain
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
 
 const val API_ORIGIN = "https://pck.shiron.dev"
 const val NEW_TARGET_URL = "${API_ORIGIN}/target/"
@@ -16,12 +18,19 @@ interface ApiInterface {
     fun getTargetData(@Body request: ReqTargetClass): Call<TargetClass>
 }
 
+private val okHttpClient = OkHttpClient.Builder()
+    .connectTimeout(60, TimeUnit.MINUTES) // 接続のタイムアウトを60秒に設定
+    .readTimeout(60, TimeUnit.MINUTES) // 読み取りのタイムアウトを60秒に設定
+    .writeTimeout(60, TimeUnit.MINUTES) // 書き込みのタイムアウトを60秒に設定
+    .build()
+
 // Retrofitクライアントを初期化します
 private val retrofit = Retrofit.Builder()
     .baseUrl(NEW_TARGET_URL)
     .addConverterFactory(GsonConverterFactory.create(
         GsonBuilder().setDateFormat("EEE, dd MMM yyyy HH:mm:ss Z").create()
     ))
+    .client(okHttpClient)
     .build()
 
 // APIを呼び出す関数を定義します

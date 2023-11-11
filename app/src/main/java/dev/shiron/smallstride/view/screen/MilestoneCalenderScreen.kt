3dc5800c4549c93cc0be62300c274fdc117c9597
shiron <shiron4710.dev@gmail.com>
2023-11-11 09:52:31 +0900

    feat: target screen 1

--- app/src/main/java/dev/shiron/smallstride/view/screen/MilestoneCalenderScreen.kt
+++ app/src/main/java/dev/shiron/smallstride/view/screen/MilestoneCalenderScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +25,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import dev.shiron.smallstride.model.TargetClass
 import dev.shiron.smallstride.ui.theme.SmallStrideTheme
+import dev.shiron.smallstride.view.component.MyScaffold
 import dev.shiron.smallstride.view.dummyTarget
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -31,78 +34,84 @@ import java.util.Calendar
 @Composable
 fun MilestoneCalenderScreen(navController: NavController, target: TargetClass, milestoneIndex: Int) {
     val milestone = target.milestones[milestoneIndex]
-    Column(
-        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 20.dp)
-    ) {
-        Text(
-            "${target.endDayAt}日で${target.title}",
-            style = TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight(400),
-                color = Color(0xFF000000)
-            )
-        )
-        Column(verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top)) {
-            Row(horizontalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterHorizontally)) {
-                Text(
-                    "Day${milestone.dayAt}",
-                    style = TextStyle(
-                        fontSize = 22.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF000000)
+    MyScaffold(navController = navController, title = "ToDoの詳細を確認") {
+        LazyColumn(contentPadding = it) {
+            item {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 20.dp)
+                ) {
+                    Text(
+                        "${target.endDayAt}日で${target.title}",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000)
+                        )
                     )
-                )
-                val cal = Calendar.getInstance()
-                cal.time = target.startDay
-                cal.add(Calendar.DATE, milestone.dayAt)
-                val format = SimpleDateFormat("MM/dd")
-                Text(
-                    format.format(cal.time),
-                    style = TextStyle(
-                        fontSize = 22.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF000000)
-                    )
-                )
-            }
-            Text(
-                milestone.title,
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight(400),
-                    color = Color(0xFF000000)
-                )
-            )
-            Column {
-                Text(
-                    text = milestone.hint,
-                    style = TextStyle(
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF000000)
-                    )
-                )
-            }
-        }
-        Column {
-            Button(
-                onClick = {
-                    navController.navigate("calender/target/${target.fileName}")
-                },
-                colors = ButtonDefaults.run { buttonColors(Color(0xFF80A8FF)) },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("目標の予定へ")
-            }
-            Button(
-                onClick = { navController.navigateUp() },
-                colors = ButtonDefaults.run { buttonColors(Color(0xFF80A8FF)) },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("戻る")
+                    Column(verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top)) {
+                        Row(horizontalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterHorizontally)) {
+                            Text(
+                                "Day${milestone.dayAt}",
+                                style = TextStyle(
+                                    fontSize = 22.sp,
+                                    fontWeight = FontWeight(400),
+                                    color = Color(0xFF000000)
+                                )
+                            )
+                            val cal = Calendar.getInstance()
+                            cal.time = target.startDay
+                            cal.add(Calendar.DATE, milestone.dayAt)
+                            val format = SimpleDateFormat("MM/dd")
+                            Text(
+                                format.format(cal.time),
+                                style = TextStyle(
+                                    fontSize = 22.sp,
+                                    fontWeight = FontWeight(400),
+                                    color = Color(0xFF000000)
+                                )
+                            )
+                        }
+                        Text(
+                            milestone.title,
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight(400),
+                                color = Color(0xFF000000)
+                            )
+                        )
+                        Column {
+                            Text(
+                                text = milestone.hint,
+                                style = TextStyle(
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight(400),
+                                    color = Color(0xFF000000)
+                                )
+                            )
+                        }
+                    }
+                    Column {
+                        Button(
+                            onClick = {
+                                navController.navigate("calender/target/${target.fileName}")
+                            },
+                            colors = ButtonDefaults.run { buttonColors(FloatingActionButtonDefaults.containerColor) },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                "目標の予定へ",
+                                style = TextStyle(
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight(400),
+                                    color = Color.DarkGray
+                                )
+                            )
+                        }
+                    }
+                }
             }
         }
     }
